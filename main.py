cc60175ba2c17ae0fa3f13c0f421bc203678acdc
Willow <NONE>
2025-02-06 22:32:44 -0600

    main now prints report on specific book

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 
-def printText():
-    path_to_file = "books/frankenstein.txt"
-
+def getText(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
-def countWords():
-    path_to_file = "books/frankenstein.txt"
+def countWords(path_to_file):
+    text = getText(path_to_file)
+    return len(text.split())
 
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(len(file_contents.split()))
+def countChars(path_to_file):
+    text = getText(path_to_file)
+    text = text.lower()
+
+    charCount = {}
+    for char in text:
+        if char in charCount:
+            charCount[char] += 1
+        else:
+            charCount[char] = 1
+    
+    return charCount
+
+def generateCharReport(path_to_file):
+    chars = countChars(path_to_file)
+    sortedChars = sorted(chars)
+
+    report = f"--- Report of {path_to_file} ---\n\n"
+    report += f"{countWords(path_to_file)} words found in file.\n\n"
+    
+    for sChar in sortedChars:
+        if sChar.isalpha():
+            report += f"The '{sChar}' char was found {chars[sChar]} times.\n"
+    report += f"\n--- End Report ---"
+    
+    return report
+
 
 if __name__ == '__main__':
-    #printText()
-    countWords()
+    path = "books/frankenstein.txt"
+
+    #print(getText(path))
+    #print(countWords(path))
+    #print(countChars(path))
+    print(generateCharReport(path))
